58829995bca3be27de571d495be62b52f8e893d9
perelokesh <lokesh7093@gmail.com>
2022-03-06 00:13:31 +0530

    clean text

---
+++ 1b.js
@@ -0,0 +1,12 @@
+/*1.b. Write a function called ***cleanText***. The function takes raw text as a parameter and returns the clean text.
+```js
+const sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`;
+    console.log(cleanText(sentence));
+`I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?`*/
+
+const sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`;
+
+function cleanText(sentence) {
+    return sentence.replace(/[^a-zA-Z. ]/g, "");
+}
+console.log(cleanText(sentence));
