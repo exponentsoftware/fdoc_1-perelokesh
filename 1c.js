f78414f7e93a1b113e0c77c42eb8eebc83c46e27
perelokesh <lokesh7093@gmail.com>
2022-03-28 22:59:20 +0530

    count words

---
+++ 1c.js
@@ -0,0 +1,13 @@
+const sentence =  `I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?`
+const countWords = (sen) => {
+    const words =sen.split(' ');
+    let count = 0;
+    for(const word of words){
+        if(word.length >1){
+            count++;
+        }
+
+    }
+    return count;
+}
+console.log(countWords(sentence));
